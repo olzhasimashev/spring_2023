34dbfcb55330d8e28202d64251edbce5eb6d19ee
unknown <imashev.olzhas@gmail.com>
2023-02-23 14:40:04 +0600

    sis1, sis2, sis3 added

---
+++ sis3/src/main/java/com/example/sis3/Sis3Application.java
@@ -0,0 +1,13 @@
+package com.example.sis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sis3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sis3Application.class, args);
+	}
+
+}
